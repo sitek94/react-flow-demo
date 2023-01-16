b737071205a44340932518525760f22195ab0b9e
Maciek Sitkowski <msitkowski94@gmail.com>
2023-01-16 22:20:09 +0100

    add interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
-import ReactFlow, {Background, Controls} from 'reactflow'
+import * as React from 'react'
+import ReactFlow, {
+  Controls,
+  Background,
+  applyNodeChanges,
+  applyEdgeChanges,
+} from 'reactflow'
+import type {Node, Edge} from 'reactflow'
+
+const initialNodes: Node[] = [
+  {
+    id: '1',
+    data: {label: 'Hello'},
+    position: {x: 0, y: 0},
+    type: 'input',
+  },
+  {
+    id: '2',
+    data: {label: 'World'},
+    position: {x: 100, y: 100},
+  },
+]
+
+const initialEdges: Edge[] = [
+  {id: '1-2', source: '1', target: '2', label: 'to the', type: 'step'},
+]
 
 function Flow() {
+  const [nodes, setNodes] = React.useState(initialNodes)
+  const [edges, setEdges] = React.useState(initialEdges)
+
   return (
-    <ReactFlow>
+    <ReactFlow
+      nodes={nodes}
+      onNodesChange={changes =>
+        setNodes(nodes => applyNodeChanges(changes, nodes))
+      }
+      edges={edges}
+      onEdgesChange={changes =>
+        setEdges(edges => applyEdgeChanges(changes, edges))
+      }
+    >
       <Background />
       <Controls />
     </ReactFlow>
