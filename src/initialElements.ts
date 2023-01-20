24401be45198098722d8664da172e73bfd33ab90
Maciek Sitkowski <msitkowski94@gmail.com>
2023-01-20 21:19:22 +0100

    generate edges based on initialNodes

--- src/initialElements.ts
+++ src/initialElements.ts
@@ -1,67 +1,89 @@
-import {Edge, MarkerType, Node} from 'reactflow'
+import {MarkerType, Node} from 'reactflow'
 
-export const initialNodes: Node[] = [
-  {
-    id: 'budget',
-    type: 'budget',
-    position: {x: 250, y: 300},
-    data: {
-      label: 'Budget',
-    },
+let id = 0
+
+const budgetNode = {
+  id: 'budget',
+  type: 'budget' as const,
+  position: {x: 250, y: 300},
+  data: {
+    label: 'Budget',
   },
+}
+
+const createIncomeNode = (amount = 0) => ({
+  id: `income-${id++}`,
+  type: 'income' as const,
+  position: {x: 250, y: 25},
+  data: {
+    amount,
+  },
+})
+
+const createExpenseNode = (amount = 0) => ({
+  id: `expense-${id++}`,
+  type: 'expense' as const,
+  position: {x: 250, y: 500},
+  data: {
+    amount,
+  },
+})
+
+export const initialNodes = [
+  budgetNode,
   {
-    id: 'expense',
-    type: 'expense',
+    ...createExpenseNode(200),
     position: {x: 180, y: 450},
-    data: {
-      amount: -100,
-    },
-    style: {
-      border: '3px solid red',
-    },
   },
   {
-    id: 'income',
-    type: 'income',
+    ...createIncomeNode(1000),
     position: {x: 220, y: 150},
-    data: {
-      amount: 100,
-    },
-    style: {
-      border: '3px solid green',
-    },
   },
 ]
 
-export const initialEdges: Edge[] = [
-  {
-    id: 'budget-expense',
-    source: 'budget',
-    target: 'expense',
-    markerEnd: {
-      type: MarkerType.Arrow,
-      width: 20,
-      height: 20,
-      color: 'red',
-    },
-    style: {
-      strokeWidth: 2,
-      stroke: 'red',
-    },
-  },
-  {
-    id: 'income-budget',
-    source: 'income',
-    target: 'budget',
-    markerEnd: {
-      type: MarkerType.Arrow,
-      width: 20,
-      height: 20,
-      color: 'green',
-    },
-    style: {
-      strokeWidth: 2,
-      stroke: 'green',
-    },
-  },
-]
+const generateEdges = (nodes: Node[]) => {
+  let edges = []
+
+  const incomeNodes = nodes.filter(node => node.type === 'income')
+  const expenseNodes = nodes.filter(node => node.type === 'expense')
+
+  for (const incomeNode of incomeNodes) {
+    edges.push({
+      id: `edge-${id++}`,
+      source: incomeNode.id,
+      target: budgetNode.id,
+      markerEnd: {
+        type: MarkerType.Arrow,
+        width: 20,
+        height: 20,
+        color: 'green',
+      },
+      style: {
+        strokeWidth: 2,
+        stroke: 'green',
+      },
+    })
+  }
+
+  for (const expenseNode of expenseNodes) {
+    edges.push({
+      id: `edge-${id++}`,
+      source: budgetNode.id,
+      target: expenseNode.id,
+      markerEnd: {
+        type: MarkerType.Arrow,
+        width: 20,
+        height: 20,
+        color: 'red',
+      },
+      style: {
+        strokeWidth: 2,
+        stroke: 'red',
+      },
+    })
+  }
+
+  return edges
+}
+
+export const initialEdges = generateEdges(initialNodes)
