20ffa16f837b391e4995e22a63a5abe4ee4008bc
Maciek Sitkowski <msitkowski94@gmail.com>
2023-01-20 21:39:25 +0100

    extract and style AddTransactionsPanel

---
+++ src/AddTransactionsPanel.tsx
@@ -0,0 +1,27 @@
+import type {Node} from 'reactflow'
+import {Panel} from 'reactflow'
+import {createExpenseNode, createIncomeNode} from './initialElements'
+import * as React from 'react'
+
+export function AddTransactionsPanel({
+  addNode,
+}: {
+  addNode: (node: Node) => void
+}) {
+  return (
+    <Panel position="top-left" className="flex gap-2">
+      <button
+        className="p-2 bg-green-500 text-white rounded shadow"
+        onClick={() => addNode(createIncomeNode())}
+      >
+        Add Income
+      </button>
+      <button
+        className="p-2 bg-red-500 text-white rounded shadow"
+        onClick={() => addNode(createExpenseNode())}
+      >
+        Add Expense
+      </button>
+    </Panel>
+  )
+}
